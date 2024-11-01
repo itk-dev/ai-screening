71e8a2c2c2d3eee638652c548abc5f3ffa7de127
Jesper Pedersen <pejes@aarhus.dk>
2024-11-01 10:59:31 +0100

    2872: Remove bad reference

--- web/themes/custom/itkdev/itkdev_base_theme/app.js
+++ web/themes/custom/itkdev/itkdev_base_theme/app.js
@@ -8,7 +8,6 @@ Alpine.start();
 // Require base theme assets
 require("./css/styles.dist.css");
 require("./js/fontawesome.js");
-require("./js/navigation.js");
 
 // Require scripts from project theme
 require("../itkdev_project_theme/js/project-scripts.js");
