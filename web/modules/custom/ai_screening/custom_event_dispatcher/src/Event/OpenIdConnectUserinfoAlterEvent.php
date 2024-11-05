06c48f2ef2920892e401e5f6f5130ef44fd2a484
Mikkel Ricky <rimi@aarhus.dk>
2024-11-05 09:40:34 +0100

    Added event usage example

--- web/modules/custom/ai_screening/custom_event_dispatcher/src/Event/OpenIdConnectUserinfoAlterEvent.php
+++ web/modules/custom/ai_screening/custom_event_dispatcher/src/Event/OpenIdConnectUserinfoAlterEvent.php
@@ -9,6 +9,29 @@ use Drupal\hook_event_dispatcher\Event\EventInterface;
 
 /**
  * OpenID Connect userinfo alter event.
+ *
+ * Example usage (note the extensive use of &, cf.
+ * https://www.php.net/manual/en/language.references.php):
+ *
+ * @code
+ * class OpenIdConnectHelper implements EventSubscriberInterface {
+ *
+ *   public function userinfoAlter(OpenIdConnectUserinfoAlterEvent $event): void {
+ *     $userinfo = &$event->getUserinfo();
+ *
+ *     if (isset($userinfo['role']) && !is_array($userinfo['groups'])) {
+ *       $userinfo['groups'] = $userinfo['role'];
+ *     }
+ *   }
+ *
+ *   public static function getSubscribedEvents(): array {
+ *     return [
+ *       OpenIdConnectHookEvents::USERINFO_ALTER => 'userinfoAlter',
+ *     ];
+ *   }
+ *
+ * }
+ * @endcode
  */
 #[HookEvent(id: 'openid_connect_userinfo_alter', alter: 'openid_connect_userinfo')]
 class OpenIdConnectUserinfoAlterEvent extends Event implements EventInterface {
