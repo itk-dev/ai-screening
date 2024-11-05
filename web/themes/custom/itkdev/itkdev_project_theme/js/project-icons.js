3ee0468d8e2613ac91bfe3ba9a9f400ca4e539e7
Sine Jespersen <sinejespersen@gmail.com>
2024-11-05 13:11:09 +0100

    2903: add create new project button (with todo) and add icon in project icons

--- web/themes/custom/itkdev/itkdev_project_theme/js/project-icons.js
+++ web/themes/custom/itkdev/itkdev_project_theme/js/project-icons.js
@@ -1,3 +1,3 @@
 export const projectFaBrandIcons = ['faTwitter'];
-export const projectFaSolidIcons = ['faAngleRight'];
+export const projectFaSolidIcons = ['faAngleRight', 'faCirclePlus'];
 export const projectFaRegularIcons = ['faAngleLeft'];
