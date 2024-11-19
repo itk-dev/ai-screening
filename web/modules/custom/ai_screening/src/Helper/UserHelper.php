9afd60e88b61510c22621f4ce89d1ba3aa59cfe9
martinyde <yde001@gmail.com>
2024-11-19 12:31:45 +0100

    Updated config

--- web/modules/custom/ai_screening/src/Helper/UserHelper.php
+++ web/modules/custom/ai_screening/src/Helper/UserHelper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\ai_screening\Helper;
 
-use AllowDynamicProperties;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannel;
