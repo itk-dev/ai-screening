a4d964d85da0055538c3428e3ccbebed718f1ea0
martinyde <yde001@gmail.com>
2024-11-22 09:47:18 +0100

    Changed stuff after code review

--- web/themes/custom/itkdev/itkdev_project_theme/tailwind.config.js
+++ web/themes/custom/itkdev/itkdev_project_theme/tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
     "font-bold",
     "bg-slate-300",
     "bg-yellow-700",
-    "bg-red-700"
+    "bg-red-700",
+    "bg-green-500",
+    "bg-red-600",
+    "bg-amber-300"
   ],
 };
