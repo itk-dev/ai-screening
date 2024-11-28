fd709fb9654391d9f29e767d9e6bf9c359aaf4f5
martinyde <yde001@gmail.com>
2024-11-28 10:42:17 +0100

    Added config pages

---
+++ web/modules/custom/ai_screening/src/Controller/SiteSetupController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ai_screening\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\node\NodeInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Returns responses for site setup page.
+ */
+final class SiteSetupController extends ControllerBase {
+
+  use AutowireTrait;
+
+  /**
+   * Builds the response for showing setup page.
+   */
+  public function __invoke(Request $request): array {
+    return [
+      '#theme' => 'site_setup',
+      '#data' => []
+    ];
+  }
+
+}
