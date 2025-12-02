83b717bb50a174a7eab8ea0bec951429bc53aa22
Jeppe Krogh <kjej@aarhus.dk>
2025-12-02 14:03:22 +0100

    fix: Break circular dependency in ProjectHelper

--- web/modules/custom/ai_screening_project/src/Helper/ProjectHelper.php
+++ web/modules/custom/ai_screening_project/src/Helper/ProjectHelper.php
@@ -111,7 +111,7 @@ class ProjectHelper extends AbstractHelper implements EventSubscriberInterface {
    */
   public function __construct(
     private readonly AccountProxyInterface $accountProxy,
-    private readonly ProjectTrackHelper $projectTrackHelper,
+    //private readonly ProjectTrackHelper $projectTrackHelper,
     private readonly ProjectTrackTypeHelper $projectTrackTypeHelper,
     EntityTypeManagerInterface $entityTypeManager,
     LoggerChannel $logger,
@@ -153,7 +153,8 @@ class ProjectHelper extends AbstractHelper implements EventSubscriberInterface {
       }
 
       $projectTracks = $this->loadProjectTracks($project);
-      $this->projectTrackHelper->deleteProjectTracks($projectTracks);
+      \Drupal::service(ProjectTrackHelper::class)->deleteProjectTracks($projectTracks);
+
     }
     catch (\Exception $exception) {
       $this->error('Error deleting project: @message', [
