a5c796a6220ed814a657963096fdee79b4e916b8
Mikkel Ricky <rimi@aarhus.dk>
2026-01-21 12:42:02 +0100

    Replaced "project" with "screening" in user-facing texts

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Create project", async ({ page }) => {
+test("Create screening", async ({ page }) => {
   await page.goto("/");
 
   await page
@@ -13,11 +13,11 @@ test("Create project", async ({ page }) => {
 
   await page.getByRole("link", { name: "Screeninger" }).click();
 
-  await page.getByRole("link", { name: "Create new project" }).click();
+  await page.getByRole("link", { name: "Create new screening" }).click();
 
-  await page.getByLabel("Title", { exact: true }).fill("My first project");
+  await page.getByLabel("Title", { exact: true }).fill("My first screening");
   await page
     .getByLabel("Description", { exact: true })
-    .fill("This is my first project.");
+    .fill("This is my first screening.");
   await page.getByRole("button", { name: "Save", exact: true }).click();
 });
